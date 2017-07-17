b231b0a5cd6fdff222330179babc0cdb81e6cba5
Han Che <hanche2001@gmail.com>
2017-07-17 19:21:12 +0200

    add favicon

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const path = require('path');
-const express = require('express')
+const http = require('http');
+const express = require('express');
+const socketIO =  require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
+io.on('connection',(socket)=>{
+    console.log('new user connected')
+})
+
+
+server.listen(3000,()=>{
     console.log(`Service is up on port ${port}`)
 })
 
